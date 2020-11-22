a8fd586c0609ab001af951da90a650f9a6a8f0a7
Gwenael Pluchon <accounts@gwenp.fr>
2020-11-22 21:03:27 +0100

    fix typos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
+const fs = require('fs');
 module.exports = {
   init (expressApp) {
     const serverHttps = require('https').createServer({
       // passphrase: 'Arson37Availed88Resolve'
     }, expressApp);
-    const isDirectory = source => !fs.lstatSync(source).isFile();
-    const certFolders = fs.readdirSync('./certificates', { withFileTypes: true })
-      .filter(e => e.isDirectory())
-      .map(d => d.name);
-
+    const isDirectory = source => !fs.lstatSync('./certificates/' + source).isFile();
+    const certFolders = fs.readdirSync('./certificates')
+      .filter(isDirectory)
+      //.map(d => d.name);
+    console.log('certFolders', certFolders);
     if (certFolders.length === 0) {
       console.warn('no certificate found. You should generate at least one certificate into the "certificates" folder (cf README.md inside the "certificates" folder.');
     }
